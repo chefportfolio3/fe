7ea090c27db2352b00ba8f5086c7e96d84713e02
msearles25 <skaman4459@gmail.com>
2020-01-06 16:26:52 -0500

    login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Login from './components/login';
 
 function App() {
   return (
     <div className="App">
+      <Login />
     </div>
   );
 }
