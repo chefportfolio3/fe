f3117aa78fbb05dd8b2d2ad821da080e7e56bc02
msearles25 <skaman4459@gmail.com>
2020-01-08 17:21:41 -0500

    login auth

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,58 +1,71 @@
 import React, { useState } from 'react';
 import { Input, Button, Dialog, Typography as Font } from '@material-ui/core';
 
-const Login = () => {
-    const [user, setUser] = useState({
-        username: '',
-        password: ''
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+
+const Login = props => {
+  const [user, setUser] = useState({
+    username: '',
+    password: ''
+  })
+  const [modal, setModal] = useState(false)
+  const history = useHistory();
+
+  const handleChange = e => {
+    setUser({
+      ...user,
+      [e.target.name]: e.target.value
     })
-    const [modal, setModal] = useState(false)
+  }
+  const handleSubmit = e => {
+    e.preventDefault();
+    // request here
+    axios.post('https://mychefportfolio.herokuapp.com/api/auth/login', user)
+      .then(res => {
+        localStorage.setItem('bwToken', res.data.token)
+      })
+    props.setToken(localStorage.getItem('bwToken'))
+    setUser({
+      username: '',
+      password: ''
+    })
+    handleClose();
+    history.push('/profile')
+  }
 
-    const handleChange = e => {
-        setUser({
-            ...user,
-            [e.target.name]: e.target.value
-        })
-    }
+  const handleClose = () => {
+    setModal(false)
+  }
 
-    const handleSubmit = e => {
-        e.preventDefault();
-
-        // request here
-    }
-
-    const handleClose = () => {
-        setModal(false)
-    }
-
-    return (
-      <>
-        <Dialog open={modal} onClose={handleClose} className="FormLogin">
-          <Font variant='h3' align='center'>Login Now!</Font>
-          <form onSubmit={handleSubmit}>
-            <Input
-              name="username"
-              placeholder="Username"
-              value={user.username}
-              onChange={handleChange}
-              required
-            />
-            <Input
-              type="password"
-              name="password"
-              placeholder="Password"
-              value={user.password}
-              onChange={handleChange}
-              required
-            />
-            <Button color="primary" variant="contained" type="submit">
-              Login
+  return (
+    <>
+      <Dialog open={modal} onClose={handleClose} className="FormLogin">
+        <Font variant='h3' align='center'>Login Now!</Font>
+        <form onSubmit={handleSubmit}>
+          <Input
+            name="username"
+            placeholder="Username"
+            value={user.username}
+            onChange={handleChange}
+            required
+          />
+          <Input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={user.password}
+            onChange={handleChange}
+            required
+          />
+          <Button color="primary" variant="contained" type="submit">
+            Login
             </Button>
-          </form>
-        </Dialog>
-        <Button onClick={() => setModal(true)}>Show Login</Button>
-      </>
-    );
+        </form>
+      </Dialog>
+      <Button onClick={() => setModal(true)}>Show Login</Button>
+    </>
+  );
 }
 
 export default Login;
