012f1a28b2a69ae27ed113eab808d31ee4cf3020
msearles25 <skaman4459@gmail.com>
2020-01-09 12:23:35 -0500

    added navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from '@material-ui/core';
+
+const Navbar = () => {
+    return (
+        <div className='main'>
+            <div className='nav-bar'>
+                <div className='nav-list'>
+                    <Link>one</Link>
+                    <Link>one</Link>
+                    <Link>one</Link>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Navbar;
